b67a607fa10ac44f507c414cfbbae7c9e5bc7816
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-08-06 19:07:57 +0530

    New user routes for updation

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { validateSignup, validateSignin } from "../middlewares/userValidation";
-import { signupUser, signinUser } from "../controller/userController";
+import { validateSignup, validateSignin, validateEmail, validateUsername } from "../middlewares/userValidation";
+import { signupUser, signinUser, updateEmail, updateUsername } from "../controller/userController";
+import verifyJWT from "../middlewares/verifyJWT";
 const userRouter = express.Router();
 
 userRouter.post("/signup", validateSignup, signupUser);
 
 userRouter.post("/signin", validateSignin, signinUser);
+
+userRouter.put("/email", verifyJWT, validateEmail, updateEmail);
+
+userRouter.put("/username", verifyJWT, validateUsername, updateUsername);
 
 export default userRouter;
