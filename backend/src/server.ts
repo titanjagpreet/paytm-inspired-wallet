14799f41597e76e3883fc8c11b1589966ba841b7
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-08-07 16:49:56 +0530

    Added new route path for account activities

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./models/db";
 import userRouter  from "./routes/userRoutes";
+import accountRouter from "./routes/accountRoutes";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -12,6 +13,8 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/account", accountRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}...`);
