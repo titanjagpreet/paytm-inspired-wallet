ff1bd09a4fd5c9e9bc43d33b7e0b356c5acd01fa
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-08-07 16:48:34 +0530

    Added account and transaction schema

--- backend/src/models/models.ts
+++ backend/src/models/models.ts
@@ -38,6 +38,79 @@ const userSchema = new Schema({
     }
 );
 
-const userModel = mongoose.model('User', userSchema);
+const accountSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+        unique: true
+    },
+    balance: {
+        type: mongoose.Schema.Types.Decimal128,
+        required: true,
+        get: (v: mongoose.Types.Decimal128) => parseFloat(v.toString()),
+    },
+    currency: {
+        type: String,
+        enum: ['INR', 'USD', 'ETH', 'BTC'],
+        default: 'INR',
+    },
+    accountType: {
+        type: String,
+        enum: ['wallet', 'savings', 'trading'],
+        default: 'wallet'
+    },
+    isFrozen: {
+        type: Boolean,
+        default: false
+    },
+},
+    {
+        timestamps: true
+    }
+);
 
-export default userModel;
+const transactionSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: true,
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: true,
+    },
+    amount: {
+        type: mongoose.Schema.Types.Decimal128,
+        required: true,
+        min: 0.01,
+        get: (v: mongoose.Types.Decimal128) => parseFloat(v.toString()),
+    },
+    currency: {
+        type: String,
+        enum: ['INR', 'USD', 'ETH', 'BTC'],
+        default: 'INR',
+    },
+    status: {
+        type: String,
+        enum: ["success", "failed", "pending"],
+        default: "pending"
+    },
+    note: {
+        type: String,
+        default: "",
+        maxlength: 100,
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
+const userModel = mongoose.model('User', userSchema);
+const accountModel = mongoose.model('Account', accountSchema);
+const txnModel = mongoose.model('Transaction', transactionSchema);
+
+export { userModel, accountModel, txnModel };
