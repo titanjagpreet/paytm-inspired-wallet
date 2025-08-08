8b7a0085d3d7b3100c8dd6a4ceacbcc509ae578b
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-08-08 17:39:29 +0530

    Added new routes to get the user's profile

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { validateSignup, validateSignin, validateEmail, validateUsername } from "../middlewares/userValidation";
-import { signupUser, signinUser, updateEmail, updateUsername } from "../controller/userController";
+import { signupUser, signinUser, getProfile, updateEmail, updateUsername } from "../controller/userController";
 import verifyJWT from "../middlewares/verifyJWT";
 const userRouter = express.Router();
 
 userRouter.post("/signup", validateSignup, signupUser);
 
 userRouter.post("/signin", validateSignin, signinUser);
+
+userRouter.get("/profile", verifyJWT, getProfile);
 
 userRouter.put("/email", verifyJWT, validateEmail, updateEmail);
 
